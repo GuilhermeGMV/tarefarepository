3a0397262a27242226e1803ba6c890bfc9b94e27
GuilhermeGMV <112208342+GuilhermeGMV@users.noreply.github.com>
2024-09-11 23:16:29 +0000

    d

---
+++ demo/src/main/java/com/bcopstein/ex1biblioeca/AcervoJDBCImpl.java
@@ -0,0 +1,30 @@
+@Repository
+@Primary
+public class AcervoJDBCImpl implements IAcervoRepository{
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public AcervoJDBCImpl(JdbcTemplate jdbcTemplate){this.jdbcTemplate = jdbcTemplate;}
+
+    @Override
+    public List<Livro> getAll(){
+        List<Livro> resp = this.jdbcTemplate.query("SELECT * from livros",
+        (rs,rowNum)->
+            new Livro(rs.getInt("codigo"),rs.getString("titulo"),rs.getString("autor"),rs.getInt("ano")));
+        return resp;
+    }
+
+    @Override
+    public boolean removeLivro(long codigo){
+        this.jdbcTemplate.update("DELETE FROM livros WHERE codigo = ?", codigo);
+        return true;
+    }
+
+    @Override
+    public boolean cadastraLivroNovo(Livro livro){
+        this.jdbcTemplate.update(
+            "INSERT INTO livros(codigo,titulo,autor,ano) VALUES (?,?,?,?)",
+            livro.codigo(),livro.titulo(),livro.autor(),livro.ano());
+            return true;
+    }
+}
